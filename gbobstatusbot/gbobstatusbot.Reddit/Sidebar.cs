2f59ebf0f433219019635bb9f7818e83d887af79
Winslow Dalpe <rwdalpe@gmail.com>
2014-11-15 21:30:03 -0800

    starting reddit classes

---
+++ gbobstatusbot/gbobstatusbot.Reddit/Sidebar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using RedditSharp;
+
+namespace gbobstatusbot.Reddit
+{
+    public class Sidebar
+    {
+        private string redditUsername;
+        private string redditPassword;
+        private RedditSharp.Reddit reddit;
+
+        public string Subreddit { get; set; }
+
+        public Sidebar (string username, string password, string subreddit)
+        {
+            redditUsername = username;
+            redditPassword = password;
+            Subreddit = subreddit;
+
+            reddit = new RedditSharp.Reddit (username, password, true);
+        }
+
+        public string CurrentText() {
+
+        }
+    }
+}
+
