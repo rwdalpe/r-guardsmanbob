b8c586bb84ffbb78506cfc9b032c974c724b0b55
Winslow Dalpe <rwdalpe@gmail.com>
2014-11-28 14:10:09 -0800

    adding a test project for the Reddit namespace

---
+++ gbobstatusbot/gbobstatusbot.Reddit.Tests/Integration Tests/Test.cs
@@ -0,0 +1,28 @@
+﻿using gbobstatusbot.Reddit;
+using NUnit.Framework;
+using System;
+using System.Configuration;
+
+namespace gbobstatusbot.Reddit.Tests.Integration_Tests
+{
+    [TestFixture ()]
+    public class Sidebar
+    {
+        private string username = "";
+        private string password = "";
+        private string subredditName = "";
+
+        [SetUp]
+        public void Initialize()
+        {
+
+        }
+
+        [Test ()]
+        [Category("Integration Tests")]
+        public void TestGetCurrentText ()
+        {
+        }
+    }
+}
+
