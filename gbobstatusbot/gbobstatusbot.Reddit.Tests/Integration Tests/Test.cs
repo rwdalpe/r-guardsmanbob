82e82ef6c772cd7df8219e73909cd8b3c58a811c
Winslow Dalpe <rwdalpe@gmail.com>
2014-11-28 14:23:35 -0800

    adding unit test for CurrentText()

--- gbobstatusbot/gbobstatusbot.Reddit.Tests/Integration Tests/Test.cs
+++ gbobstatusbot/gbobstatusbot.Reddit.Tests/Integration Tests/Test.cs
@@ -8,20 +8,23 @@ namespace gbobstatusbot.Reddit.Tests.Integration_Tests
     [TestFixture ()]
     public class Sidebar
     {
-        private string username = "";
-        private string password = "";
-        private string subredditName = "";
+        private string username = ConfigurationManager.AppSettings["username"];
+        private string password = ConfigurationManager.AppSettings["password"];
+        private string subredditName = ConfigurationManager.AppSettings["subreddit"];
+        private gbobstatusbot.Reddit.Sidebar sidebar;
 
         [SetUp]
         public void Initialize()
         {
-
+            sidebar = new gbobstatusbot.Reddit.Sidebar (username, password, subredditName);
         }
 
         [Test ()]
         [Category("Integration Tests")]
         public void TestGetCurrentText ()
         {
+            string desc = sidebar.CurrentText ();
+            Assert.IsTrue (desc.Contains ("Stream Status"));
         }
     }
 }
